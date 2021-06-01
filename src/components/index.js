2755dc2eda10b02367b83a04157b7a7746125848
csagastegui59 <csagastegui59@gmail.com>
2021-05-31 23:08:56 -0500

    add: components

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+const SectionHeader = styled.div`
+padding: 32 16 8 16;
+`;
+
+const NoticeCell = styled.div`
+text-align: center;
+padding: 16 16 16 16;
+`
+const LoadingCell = styled.div`
+display: flex;
+justify-content: center;
+padding: 16 16 16 16;
+`
+
+const PersonCell = styled.div`
+display: flex;
+flex-direction: column;
+margin: 16 16 16 16;
+`
+const DataCell = styled.div`
+display: flex;
+padding: 16 16 16 16;
+gap: 16;
