68550d7e920c6ef46cd340621490a40b5298c4e1
csagastegui59 <csagastegui59@gmail.com>
2023-05-26 14:00:10 -0500

    Fix issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { ApolloProvider } from '@apollo/client/react';
 import client from './client.js';
